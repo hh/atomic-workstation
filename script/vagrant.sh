97895f99f4df4f04efd1842cf616150b9cbd5356
Felix Krull <f_krull@gmx.de>
2018-03-24 22:27:07 +0100

    Don't set root password and SSH key in kickstart file

--- script/vagrant.sh
+++ script/vagrant.sh
@@ -3,6 +3,7 @@
 echo "==> Configuring settings for vagrant"
 
 SSH_USER=${SSH_USER:-vagrant}
+SSH_PASSWORD=${SSH_PASSWORD:-vagrant}
 SSH_USER_HOME=${SSH_USER_HOME:-/home/${SSH_USER}}
 VAGRANT_INSECURE_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key"
 
@@ -14,8 +15,9 @@ if [ "$INSTALL_VAGRANT_KEY" = "true" ] || [ "$INSTALL_VAGRANT_KEY" = "1" ]; then
     echo '==> Creating ${SSH_USER}'
     /usr/sbin/groupadd ${SSH_USER}
     /usr/sbin/useradd ${SSH_USER} -g ${SSH_USER}
-    echo "${SSH_USER}"|passwd --stdin ${SSH_USER}
+    echo "${SSH_PASSWORD}"|passwd --stdin ${SSH_USER}
   fi
+  echo "${SSH_PASSWORD}" | passwd --stdin root
 
   # Give Vagrant user permission to sudo
   echo "Defaults:${SSH_USER} !requiretty" > /etc/sudoers.d/vagrant
