652cf3df9f045b3af8e8672c92ea92fdca232021
Felix Krull <f_krull@gmx.de>
2018-03-24 17:05:40 +0100

    Update custom-script.sh template

--- custom-script.sh
+++ custom-script.sh
@@ -2,8 +2,6 @@
 
 set -eux
 
-# Sample custom configuration script - add your own commands here
-# to add some additional commands for your environment
-#
-# For example:
-# dnf install -y curl wget git tmux firefox xvfb
+# Template custom configuration script -- add your own commands to customise
+# the new box. Keep in mind that you can't use dnf and only selected file system
+# locations are writable.
