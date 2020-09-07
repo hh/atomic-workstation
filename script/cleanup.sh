05ddf80dc1079c06875e7ef88d98cfd4c96db1b8
Felix Krull <f_krull@gmx.de>
2020-09-07 16:47:36 +0200

    Revert "Clear swap partition by partition UUID"
    This reverts commit 58fddd2d3aaeee35af673701fb21bebe723e4932.

--- script/cleanup.sh
+++ script/cleanup.sh
@@ -14,18 +14,18 @@ ostree prune
 echo "==> Removing temporary files used to build box"
 rm -rf /tmp/*
 
+echo '==> Clear out swap and disable until reboot'
 set +e
-swapuuid=$(/sbin/blkid -o value -l -s PARTUUID -t TYPE=swap)
+swapuuid=$(/sbin/blkid -o value -l -s UUID -t TYPE=swap)
 case "$?" in
 	2|0) ;;
 	*) exit 1 ;;
 esac
 set -e
 if [ "x${swapuuid}" != "x" ]; then
-    echo '==> Clear out swap and disable until reboot'
     # Whiteout the swap partition to reduce box size
     # Swap is disabled till reboot
-    swappart=$(readlink -f /dev/disk/by-partuuid/$swapuuid)
+    swappart=$(readlink -f /dev/disk/by-uuid/$swapuuid)
     /sbin/swapoff "${swappart}"
     dd if=/dev/zero of="${swappart}" bs=1M || echo "dd exit code $? is suppressed"
     /sbin/mkswap -U "${swapuuid}" "${swappart}"
