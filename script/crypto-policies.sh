0d75d6bb89afc66e5740a41c9d9eab006af3e9d1
Felix Krull <f_krull@gmx.de>
2021-03-16 23:28:51 +0100

    Update IoT and fix crypto

--- script/crypto-policies.sh
+++ script/crypto-policies.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -eux
 if [ "x${CRYPTO_POLICY}" != "x" ]; then
+    if [ ! $(which update-crypto-policies > /dev/null) ]; then
+        echo "==> Installing crypto-policies-scripts"
+        rpm-ostree install crypto-policies-scripts
+        rpm-ostree ex livefs
+    fi
     echo "==> Setting system crypto policy"
     update-crypto-policies --set ${CRYPTO_POLICY}
 fi
